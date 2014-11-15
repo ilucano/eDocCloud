db2856f161b92d77f28950a1d3a381972ec83c76
Sam Wong <sam.wong@caterhammedia.com>
2014-11-16 01:27:22 +0800

    create group function

--- lib/data/groups/dGroups.actions.e.php
+++ lib/data/groups/dGroups.actions.e.php
@@ -134,8 +134,8 @@
 					
 					
 					$update_query = "UPDATE groups ";
-					$update_query .= "SET nombre = '" . addslashes($_GET['nombre']) . " '";
-					$update_query .= ", group_permission = '" . addslashes($json_group_permission) . " '";
+					$update_query .= "SET nombre = '" . addslashes($_GET['nombre']) . "' ";
+					$update_query .= ", group_permission = '" . addslashes($json_group_permission) . "' ";
 					$update_query .= " WHERE row_id = '".addslashes($_GET['id']) ."'";
 				    $stmt = $con->prepare($update_query);
 					//
