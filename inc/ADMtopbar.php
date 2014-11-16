9459fa80c215f0e5d17a2487a5392b14e0ebf8d2
Sam Wong <sam.wong@caterhammedia.com>
2014-11-16 17:28:46 +0800

    checking user permission

--- inc/ADMtopbar.php
+++ inc/ADMtopbar.php
@@ -30,7 +30,6 @@ $group_permission = GetUserPermission();
 	  echo '</ul></li>';
 	  }
 ?>
-    <?php print_r($group_permission) ;?>
 	
 	<?php if( is_array($group_permission['workflow']) && count($group_permission['workflow']) > 0): ?>
        <!-- work flow -->
@@ -61,38 +60,77 @@ $group_permission = GetUserPermission();
 	<?php endif ?>
  
 	<?php if( is_array($group_permission['reports']) && count($group_permission['reports']) > 0): ?>
-       <!-- work flow -->
+       <!-- reports-->
 	   
       <li class="has-dropdown">
         <a href="#">Reports</a>
         <ul class="dropdown">
-          <li<?php if ($page=="inproc") { echo ' class="active"'; } ?>><a href="inproc.php">All Boxes</a></li>  
-          <li<?php if ($page=="report01") { echo ' class="active"'; } ?>><a href="report01.php">Group By Status</a></li>  
+			
+			<?php if($group_permission['reports']['all_boxes'] == 1 ): ?>
+				<li<?php if ($page=="inproc") { echo ' class="active"'; } ?>><a href="inproc.php">All Boxes</a></li>
+	        <?php endif ?>
+			
+			<?php if($group_permission['reports']['group_by_status'] == 1 ): ?>
+				<li<?php if ($page=="report01") { echo ' class="active"'; } ?>><a href="report01.php">Group By Status</a></li>
+			<?php endif ?>
         </ul>
       </li>
 	  
 	<?php endif ?>
 	
-      <li class="has-dropdown">
-        <a href="#">Admin Menu</a>
-        <ul class="dropdown">
-          <li<?php if ($page=="main") { echo ' class="active"'; } ?>><a href="main.php">Home</a></li>
-          <li<?php if ($page=="company") { echo ' class="active"'; } ?>><a href="company.php">Company</a></li>
-          <li<?php if ($page=="users") { echo ' class="active"'; } ?>><a href="users.php">Users</a></li>
-		  <li<?php if ($page=="groups") { echo ' class="active"'; } ?>><a href="groups.php">Groups</a></li>
-          <li<?php if ($page=="orders") { echo ' class="active"'; } ?>><a href="orders.php">Orders</a></li>
-          <li<?php if ($page=="pickup") { echo ' class="active"'; } ?>><a href="pickup.php">Pickup</a></li>
-          <li<?php if ($page=="box") { echo ' class="active"'; } ?>><a href="box.php">Box</a></li>
-          <li<?php if ($page=="chart") { echo ' class="active"'; } ?>><a href="chart.php">Chart</a></li>
-          <li<?php if ($page=="file") { echo ' class="active"'; } ?>><a href="file.php">File</a></li>
-          <li<?php if ($page=="barcode") { echo ' class="active"'; } ?>><a href="barcode.php">Barcode</a></li>
-          
-          <!-- <li<?php if ($page=="search") { echo ' class="active"'; } ?>><a href="search.php">Search</a></li>
-          <li<?php if ($page=="chgpwd") { echo ' class="active"'; } ?>><a href="chgpwd.php">Change Password</a></li> -->
-        </ul>
-      </li>
+	
+		<?php if( is_array($group_permission['admin_menu']) && count($group_permission['admin_menu']) > 0): ?>
+	
+		<li class="has-dropdown">
+		  <a href="#">Admin Menu</a>
+		  <ul class="dropdown">
+			<?php if($group_permission['admin_menu']['home'] == 1 ): ?>
+				<li<?php if ($page=="main") { echo ' class="active"'; } ?>><a href="main.php">Home</a></li>
+	        <?php endif ?>
+			
+			<?php if($group_permission['admin_menu']['company'] == 1 ): ?>
+				<li<?php if ($page=="company") { echo ' class="active"'; } ?>><a href="company.php">Company</a></li>
+			<?php endif ?>
+			
+			<?php if($group_permission['admin_menu']['users'] == 1 ): ?>
+				<li<?php if ($page=="users") { echo ' class="active"'; } ?>><a href="users.php">Users</a></li>
+			<?php endif ?>
+			
+			<?php if($group_permission['admin_menu']['groups'] == 1 ): ?>
+				<li<?php if ($page=="groups") { echo ' class="active"'; } ?>><a href="groups.php">Groups</a></li>
+			<?php endif ?>
+			
+			<?php if($group_permission['admin_menu']['orders'] == 1 ): ?>
+				<li<?php if ($page=="orders") { echo ' class="active"'; } ?>><a href="orders.php">Orders</a></li>
+			<?php endif ?>
+			
+			<?php if($group_permission['admin_menu']['pickup'] == 1 ): ?>
+				<li<?php if ($page=="pickup") { echo ' class="active"'; } ?>><a href="pickup.php">Pickup</a></li>
+			<?php endif ?>
+			
+			<?php if($group_permission['admin_menu']['box'] == 1 ): ?>
+				<li<?php if ($page=="box") { echo ' class="active"'; } ?>><a href="box.php">Box</a></li>
+			<?php endif ?>
+			
+			<?php if($group_permission['admin_menu']['chart'] == 1 ): ?>
+				<li<?php if ($page=="chart") { echo ' class="active"'; } ?>><a href="chart.php">Chart</a></li>
+			<?php endif ?>
+			
+			<?php if($group_permission['admin_menu']['file'] == 1 ): ?>
+				<li<?php if ($page=="file") { echo ' class="active"'; } ?>><a href="file.php">File</a></li>
+			<?php endif ?>
+			
+			<?php if($group_permission['admin_menu']['barcode'] == 1 ): ?>
+				<li<?php if ($page=="barcode") { echo ' class="active"'; } ?>><a href="barcode.php">Barcode</a></li>
+			<?php endif ?>
+			
+			<!-- <li<?php if ($page=="search") { echo ' class="active"'; } ?>><a href="search.php">Search</a></li>
+			<li<?php if ($page=="chgpwd") { echo ' class="active"'; } ?>><a href="chgpwd.php">Change Password</a></li> -->
+		  </ul>
+		</li>
+		<?php endif ?>
     </ul>
-
+    
     <!-- Left Nav Section -->
     <ul class="left">
       <li><a href="../logout.php">Logout</a></li>
