5742fc32db25a4ac2318566aa5c740eece933e9d
Sam Wong <sam.wong@caterhammedia.com>
2014-12-06 20:22:14 +0800

    Company admin and user

--- lib/data/myusers/dMyusers.php
+++ lib/data/myusers/dMyusers.php
@@ -22,7 +22,7 @@
 	
 	$btnFirst = "<div class=\"row\">";
 	$btnCreate = "<div class=\"large-2 columns\"><a href=\"#\" class=\"button tiny\" data-type=\"create\" data-page=\"\" data-reveal-id=\"buttons\">Create</a></div>";
-	$btnSearch = "<div class=\"large-6 columns\"><div class=\"row collapse\"><div class=\"small-10 columns\"><input type=\"text\" id=\"txtsearch\" name=\"txtsearch\" placeholder=\"Enter search text here\" value=\"".$txtSearch."\"></div> <div class=\"small-2 columns\"><a href=\"#\" class=\"button postfix\" data-type=\"pagina\" data-page=\"\" data-reveal-id=\"grill\">Search</a></div></div><div class=\"large-4 columns\"></div></div>";
+	$btnSearch = "<div  style=\"display:none;\" class=\"large-6 columns\"><div class=\"row collapse\"><div class=\"small-10 columns\"><input type=\"text\" id=\"txtsearch\" name=\"txtsearch\" placeholder=\"Enter search text here\" value=\"".$txtSearch."\"></div> <div class=\"small-2 columns\"><a href=\"#\" class=\"button postfix\" data-type=\"pagina\" data-page=\"\" data-reveal-id=\"grill\">Search</a></div></div><div class=\"large-4 columns\"></div></div>";
 	$btnLast = "</div>";
 	$antes = "<div class=\"large-1 columns\">&nbsp;</div><div class=\"large-10 columns\">";
 	$despues = "</div><div class=\"large-1 columns\"></div></div>";
@@ -40,44 +40,42 @@
 	
 	$array_bind[':fk_empresa'] = $companyCode;
 	
-	$users_list = $objUsers->listUsers($filter, $array_bind);
-	
-	print_r($users_list);
+	$res = $objUsers->listUsers($filter, $array_bind);
 	
 	$con = ConnectionFactory::getConnection();
 		
 	// COMIENZO DEL CAMBIO
-	if ($txtSearch!="") {
-		$qryAdd = " WHERE first_name LIKE '%".$txtSearch."%' OR last_name LIKE '%".$txtSearch."%'";
-	}
-	$qryCnt = "SELECT COUNT(*) as num FROM users ".$qryAdd;
+	//if ($txtSearch!="") {
+	//	$qryAdd = " WHERE first_name LIKE '%".$txtSearch."%' OR last_name LIKE '%".$txtSearch."%'";
+	//}
+	//$qryCnt = "SELECT COUNT(*) as num FROM users ".$qryAdd;
 	// FIN DEL CAMBIO
 		
-	$total_pages = mysql_fetch_array(mysql_query($qryCnt));
-	$total_pages = $total_pages['num'];
+	//$total_pages = mysql_fetch_array(mysql_query($qryCnt));
+	//$total_pages = $total_pages['num'];
 	//echo $total_pages;
 		
 	// COMIENZO DEL CAMBIO
-	if ($total_pages>$limit || $pagAct > 0) {
-		if ($pagAct==0) {
-			$qryFT = "SELECT T1.*, (T2.company_name) as empresa FROM users T1 INNER JOIN companies T2 ON T1.fk_empresa = T2.row_id ".$qryAdd." LIMIT ".($pagAct * $limit).",".($limit).";";
-		} else {
-			$qryFT = "SELECT T1.*, (T2.company_name) as empresa FROM users T1 INNER JOIN companies T2 ON T1.fk_empresa = T2.row_id ".$qryAdd." LIMIT ".(($pagAct) * $limit).",".($limit).";";
-		}
-	} else {
-		$qryFT = "SELECT T1.*, (T2.company_name) as empresa FROM users T1 INNER JOIN companies T2 ON T1.fk_empresa = T2.row_id ".$qryAdd.";";
-	}
+	//if ($total_pages>$limit || $pagAct > 0) {
+	//	if ($pagAct==0) {
+	//		$qryFT = "SELECT T1.*, (T2.company_name) as empresa FROM users T1 INNER JOIN companies T2 ON T1.fk_empresa = T2.row_id ".$qryAdd." LIMIT ".($pagAct * $limit).",".($limit).";";
+	//	} else {
+	//		$qryFT = "SELECT T1.*, (T2.company_name) as empresa FROM users T1 INNER JOIN companies T2 ON T1.fk_empresa = T2.row_id ".$qryAdd." LIMIT ".(($pagAct) * $limit).",".($limit).";";
+	//	}
+	//} else {
+	//	$qryFT = "SELECT T1.*, (T2.company_name) as empresa FROM users T1 INNER JOIN companies T2 ON T1.fk_empresa = T2.row_id ".$qryAdd.";";
+	//}
 	// FIN DEL CAMBIO
 	
 	//echo $qryFT;
-	mysql_query("SET NAMES UTF8");
-	$res = mysql_query($qryFT);
+	//mysql_query("SET NAMES UTF8");
+	//$res = mysql_query($qryFT);
 	
-	if (mysql_num_rows($res)) {
+	if (count($res) >= 1) {
 		// COMIENZO DEL CAMBIO
 		echo "<table><tbody><thead><tr><th width=\"20%\">Name</th><th width=\"20%\">Company</th><th width=\"20%\">Email</th><th width=\"7%\">Admin</th><th width=\"7%\">Active</th><th width=\"6%\">Comp. Adm.</th><th width=\"20%\">Actions</th></tr></thead>";
 		
-		while ($row = mysql_fetch_array($res)) {
+		foreach ($res as $row) {
 			echo "<tr><td width=20%>";
 			echo $row['first_name'].' '.$row['last_name'].' ('.$row['username'].')'.'</td>';
 			echo '<td width="20%">'.$row['empresa'].'</td>';
