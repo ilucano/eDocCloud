8ccc113d4549d6d95a52a5f8e3bf20ecc655da94
Sam Wong <sam.wong@caterhammedia.com>
2014-12-10 23:05:01 +0800

    files marker

--- orders.php
+++ orders.php
@@ -62,6 +62,15 @@ require $arrIni['base'].'inc/topbar.php';
     </table>
     
     </div>
+	<a href="#" data-reveal-id="myModal">Click Me For A Modal</a>
+
+<div id="myModal" class="reveal-modal" data-reveal>
+  <h2>Awesome. I have it.</h2>
+  <p class="lead">Your couch.  It is mine.</p>
+  <p>I'm a cool paragraph that lives inside of an even cooler modal. Wins!</p>
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+
 	<div class="large-6 columns">
 		<div class="row" id="contajax">&nbsp;</div>
     </div>
@@ -85,7 +94,7 @@ require $arrIni['base'].'inc/topbar.php';
 
   	<script>
     	$(document).foundation();
-		$(document).on("click", "a[data-reveal-id]", function() {
+		$(document).on("click", "a[data-reveal-idxx]", function() {
     		
 			
 			vId=$(this).attr('data-reveal-id');
