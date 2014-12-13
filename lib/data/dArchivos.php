614aa1337f440946a9eae073753da61099ba77b2
Sam Wong <sam.wong@caterhammedia.com>
2014-12-13 22:58:52 +0800

    file markers

--- lib/data/dArchivos.php
+++ lib/data/dArchivos.php
@@ -131,7 +131,7 @@ function dropDownButton($row_id, $mark_id)
 					if(html != "")
 					{
 						$("#set-filemark-button"+vFileId).html(html);
-						$(this).parent().parent().removeClass("open");
+						$("ul.f-dropdown").removeClass("open");
 					}
 					else
 					{
