8d5cbbe10fd65737dea3dd00b839c0703ba77a76
Sam Wong <sam.wong@caterhammedia.com>
2014-12-16 22:49:40 +0800

    taxonomy

--- lib/filebrowser/browser.php
+++ lib/filebrowser/browser.php
@@ -107,25 +107,86 @@ switch ($action)
 
 	case "listfile":
 		
-		$row = $objFiles->listFileByAlphabet($companyCode, $year, $alphabet);
+		$result = $objFiles->listFileByAlphabet($companyCode, $year, $alphabet);
 	    
 		
 		$list_result = '';
+				
 		
-		foreach($row as $list)
+		$group_permission = GetUserPermission();
+		
+		$show_file_marker  = ($group_permission['use_file_marker']['view'] == 1 || $group_permission['use_file_marker']['change'] == 1) ? true : false;
+		
+		if($show_file_marker == true) {
+			$marker_header = '<th>Marks</th>';
+		}
+		
+ 
+		foreach($result as $row)
 		{
-			$filename = $list['filename'];
-			 
-			$list_result .= '<li><a href="#">'. $filename .'</a></li>';
+		    
+			$str_filename = '<a href="lib/data/file.download.php?fileid='.$row['row_id'].'" target="_blank">'.$row['filename'].'</a>';
+
+			
+			$tr = '<tr>
+                <td>%filename%</td>';
+				
+			if($show_file_marker == true) {
+				$tr .= '<td>%marker%</td>';
+				$str_marker = dropDownButton($row['row_id'], $row['file_mark_id']);
+			}
+			$tr .= '<td>%creation_date%</td>
+					<td>%modification_date%</td>
+					<td>%pages%</td>
+					<td>%size%</td>
+				</tr>';
+			
+			
+			$bytes = $row['filesize'];
+
+			if ($bytes < 1048576) {
+				$bytes = number_format($row['filesize'] / 1024,2).' Kb';
+			} else {
+				$bytes = number_format($row['filesize'] / 1024 / 1024,2).' Mb';
+			}
+			
+			$list_result .= str_replace(array('%filename%',
+									'%marker%',
+									'%creation_date%',
+									'%modification_date%',
+									'%pages%',
+									'%size%'),
+							  array($str_filename,
+									$str_marker,
+									date("m/d/Y G:i:s",strtotime($row['creadate'])),
+									date("m/d/Y G:i:s",strtotime($row['moddate'])),
+									$row['pages'],
+									$bytes
+									),
+							  $tr);
 			
 		}
-		 
-		$html = '<h4>List of files</h3>';
 		
-		$html .= '<ul class="inline-list">
-					%list_result%
-				  </ul>';
+		$html = '<h4>List of files</h3>';
+		$html .= '<table id="list-file-table" class="display" cellspacing="0" width="100%">';
+		
+		$html .= '<thead>
+					<tr>
+						<th>Filename</th>' . $marker_header . '
+						<th>Creation Date</th>
+						<th>Modifcation Date</th>
+						<th>Pages</th>
+						<th>Size</th>
+					</tr>
+				</thead>';
+
+        $html .= '<tbody>';
+		
+		$html .= '%list_result%';
 				  
+		$html .= '</tbody>';
+		
+		$html .= '</table>';
 		
 		$html = str_replace(array('%list_result%'),
 							array($list_result),
@@ -141,6 +202,67 @@ switch ($action)
 	
 }
 
+
+
+function dropDownButton($row_id, $mark_id)
+{
+	
+	$group_permission = GetUserPermission();
+	
+	$objFilemarks = new Filemarks;
+	$objUsers = new Users;
+	$label = $objFilemarks->getLabelById($mark_id);
+	
+	if($label == '')
+	{
+		$label = "(No Mark)";
+	}
+	
+	$drop_down_list = '';
+	$disabled_class = "disabled secondary ";
+	
+    if( $group_permission['use_file_marker']['change'] == 1 ) {
+		
+			$disabled_class = '';
+			
+			$filter = " AND global = :global";
+			
+			$array_bind[':global'] = '1'; //fk_empresa = global share
+			
+			$res = $objFilemarks->listFilemarks($filter, $array_bind);
+			
+			$company_filter = " AND fk_empresa = :fk_empresa AND global = :global";
+			
+			$company_array_bind[':fk_empresa'] =  $objUsers->userCompany();
+			
+			$company_array_bind[':global'] = '0';
+			
+			$company_res = $objFilemarks->listFilemarks($company_filter, $company_array_bind);
+			
+		    
+			
+			if (count($res) >= 1 || count($company_res) >= 1) {
+				
+				foreach ($res as $row) {
+		
+					$drop_down_list .= '<li><a class="set-filemarker" data-set-filemark-id="'.$row_id.'" data-set-filemark-value="'.$row['id'].'">'.$row['label'].'</a></li>';
+					
+				}
+				
+				foreach ($company_res as $row) {
+					$drop_down_list .= '<li><a class="set-filemarker" data-set-filemark-id="'.$row_id.'" data-set-filemark-value="'.$row['id'].'">'.$row['label'].'</a></li>';
+				}
+
+				$drop_down_list .= '<li><a class="set-filemarker" data-set-filemark-id="'.$row_id.'" data-set-filemark-value=""> <i>Remove Mark</i></a></li>';
+	
+			}
+			$drop_down_list = '<ul id="drop'.$row_id.'" data-dropdown-content class="f-dropdown" aria-hidden="true" tabindex="-1">'.$drop_down_list.'</ul>';
+	
+	}
+
+	return '<button id="set-filemark-button'.$row_id.'" href="#" data-dropdown="drop'.$row_id.'" aria-controls="drop'.$row_id.'" aria-expanded="false" class="'.$disabled_class.'tiny button dropdown">'.$label.'</button><br>' .$drop_down_list;
+
+}
 
 exit();
 
